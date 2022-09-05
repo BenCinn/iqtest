573da8cb155e75234fd80447bd448225bef1f5e5
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-09-05 13:50:58 +0000

    Finish most of the most stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [preprocess({postcss: true,}),],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    }),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
